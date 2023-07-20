7fadf5aca77e4fda49cf525361155e0be8c15b05
Nader Kaake <nader.kaake@gmail.com>
2023-07-20 10:33:44 +0200

    Added kiesgrube

--- BraEvents/urls.py
+++ BraEvents/urls.py
@@ -1,15 +1,12 @@
-""" BrighterVodka URL Configuration """
-from django.contrib import admin
+""" BRA Events URL Configuration """
+# from django.contrib import admin
 from django.urls import path
-from app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from app import views
 
 urlpatterns = [
-    # General pages
     path('', views.home, name='home'),
-
-    
     path('Impressum', views.imprint, name='imprint'),
     path('Datenschutz', views.dataprivacy, name='dataprivacy'),
 ]
